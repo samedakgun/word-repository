dcb0b6020b34adc67c28515e3018d207f6cc6707
Samed Akgun <samedakgun88@gmail.com>
2024-03-23 23:59:04 +0300

    Initial Commit

---
+++ Dil Pratik/Dil_PratikApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dil_PratikApp.swift
+//  Dil Pratik
+//
+//  Created by Samed Akgün on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dil_PratikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
